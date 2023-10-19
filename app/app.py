60f1af7e004ab002cecc15ae050b05cbc9454c74
gyerekesmarcsello <marcell.lenkei@gmail.com>
2023-10-19 14:05:50 +0200

    initial

---
+++ app/app.py
@@ -0,0 +1,14 @@
+import time
+import random
+
+from sqlalchemy import create_engine
+
+db_name = 'database'
+db_user = 'username'
+db_pass = 'secret'
+db_host = 'db'
+db_port = '5432'
+
+# Connecto to the database
+db_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
+db = create_engine(db_string)
